832eff970d895fabd0bceecc6f33792a1632bf54
CW2 <CW2@atlas.cz>
2016-08-26 20:24:04 +0200

    Reorganized and cleaned up symbols in platform_selector.h

--- Solutions/STM32F746NUCLEO/platform_selector.h
+++ Solutions/STM32F746NUCLEO/platform_selector.h
@@ -1,9 +1,26 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Microsoft .NET Micro Framework Porting Kit Code Samples and is unsupported.
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use these files except in compliance with the License.
+// You may obtain a copy of the License at:
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+// 
+// Based on the Implementation for (STM32F4) by Oberon microsystems, Inc.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #ifndef _PLATFORM_STM32F746NUCLEO_SELECTOR_H_
 #define _PLATFORM_STM32F746NUCLEO_SELECTOR_H_
 
 /////////////////////////////////////////////////////////
 //
-// processor and features
+// Processor and features
 //
 
 #if defined(PLATFORM_ARM_STM32F746NUCLEO)
@@ -14,131 +31,107 @@
 
 #define STM32F746xx
 
-#define USB_ALLOW_CONFIGURATION_OVERRIDE 1
-
 //
-// processor and features
+// Processor and features
 //
 /////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////
 //
-// constants
+// Constants
 //
 
-// System clock
-#define SYSTEM_CLOCK_HZ                 192000000   // 192 MHz
-#define SYSTEM_CYCLE_CLOCK_HZ           192000000   // 192 MHz
-#define SYSTEM_APB1_CLOCK_HZ             48000000   //  48 MHz
-#define SYSTEM_APB2_CLOCK_HZ             96000000   //  96 MHz
+// System Clock
+#define SYSTEM_CLOCK_HZ                  192000000  // 192 MHz
+#define SYSTEM_CYCLE_CLOCK_HZ            192000000  // 192 MHz
+#define SYSTEM_APB1_CLOCK_HZ              48000000  //  48 MHz
+#define SYSTEM_APB2_CLOCK_HZ              96000000  //  96 MHz
 
 // FIXME: HSEBYP is not currently supported
-#define SYSTEM_CRYSTAL_CLOCK_HZ           8000000   // 8 MHz external clock (MCO from ST-LINK)
+#define SYSTEM_CRYSTAL_CLOCK_HZ            8000000  // 8 MHz external clock (MCO from ST-LINK)
 
-#define CLOCK_COMMON_FACTOR               1000000   // GCD(SYSTEM_CLOCK_HZ, 1M)
+#define CLOCK_COMMON_FACTOR                1000000  // GCD(SYSTEM_CLOCK_HZ, 1M)
 
-#define SLOW_CLOCKS_PER_SECOND            1000000   // 1 MHz
-#define SLOW_CLOCKS_TEN_MHZ_GCD           1000000   // GCD(SLOW_CLOCKS_PER_SECOND, 10M)
-#define SLOW_CLOCKS_MILLISECOND_GCD          1000   // GCD(SLOW_CLOCKS_PER_SECOND, 1k)
-
-#define SUPPLY_VOLTAGE_MV                    3300   // 3.3V supply
+#define SLOW_CLOCKS_PER_SECOND             1000000  // 1 MHz
+#define SLOW_CLOCKS_TEN_MHZ_GCD            1000000  // GCD(SLOW_CLOCKS_PER_SECOND, 10M)
+#define SLOW_CLOCKS_MILLISECOND_GCD           1000  // GCD(SLOW_CLOCKS_PER_SECOND, 1k)
 
 // Memory
 #define FLASH_MEMORY_Base               0x08000000
 #define FLASH_MEMORY_Size               0x00100000  // 1M
-
-// DTCM     0x20000000 - 0x2000FFFF  64K
-// SRAM1    0x20010000 - 0x2004BFFF 240K
-// SRAM2    0x2004C000 - 0x2004FFFF  16K
 #define SRAM1_MEMORY_Base               0x20000000
 #define SRAM1_MEMORY_Size               0x00050000  // 320K
+//      DTCM               0x20000000 - 0x2000FFFF  //  64K
+//      SRAM1              0x20010000 - 0x2004BFFF  // 240K
+//      SRAM2              0x2004C000 - 0x2004FFFF  //  16K
 
-// GPIO
-#define GPIO_PORTA 0
-#define GPIO_PORTB 1
-#define GPIO_PORTC 2
-#define GPIO_PORTD 3
-#define GPIO_PORTE 4
-#define GPIO_PORTF 5
-#define GPIO_PORTG 6
-// PH0 and PH1 intentionally omitted
+// System Configuration
 
-#define TOTAL_GPIO_PORT                 (GPIO_PORTG + 1)
-#define TOTAL_GPIO_PINS                 (TOTAL_GPIO_PORT*16)
+#define SUPPLY_VOLTAGE_MV               3300  // 3.3V supply
 
-#define PORT_PIN(port,pin)              (((int)port)*16 + (pin))
+#define TOTAL_GENERIC_PORTS             1 // ITM channel 0
 
-// Communication
-
-#define TOTAL_USART_PORT                2 // FIXME: 3
-
-#define USART_DEFAULT_PORT              COM1
-#define USART_DEFAULT_BAUDRATE          115200
-
-#define TOTAL_GENERIC_PORTS             1 // 1 generic port extensions (ITM channel 0 )
-#define ITM_GENERIC_PORTNUM             0 // ITM0 is index 0 in generic port interface table
-
-// FIXME: Link how to view ITM output
+#define TOTAL_USB_CONTROLLER            1
+#define USB_MAX_QUEUES                  4 // 4 endpoints (EP0 + 3)
+#define USB_ALLOW_CONFIGURATION_OVERRIDE 1
 
 #define DEBUG_TEXT_PORT                 ITM0
 #define STDIO                           ITM0
 #define DEBUGGER_PORT                   USB1
 #define MESSAGING_PORT                  USB1
 
-#define TOTAL_USB_CONTROLLER            1
-#define USB_MAX_QUEUES                  4  // 4 endpoints (EP0 + 3)
+#define USART_DEFAULT_PORT              COM1
+#define USART_DEFAULT_BAUDRATE          115200
 
 // System Timer Configuration
 #define STM32F4_32B_TIMER 2
 #define STM32F4_16B_TIMER 3
 
-// COM1 (0) - USART1 (APB2)
-// COM2 (1) - USART2 (APB1)
+// Peripherals and Pin Configuration
 
-//                              USART1 (APB2)             USART2 (ABP1)
-#define STM32F7_UART_TXD_PINS { PORT_PIN(GPIO_PORTA,  9), PORT_PIN(GPIO_PORTA, 2) }
-#define STM32F7_UART_RXD_PINS { PORT_PIN(GPIO_PORTA, 10), PORT_PIN(GPIO_PORTA, 3) }
+// General Purpose I/O
+#define GPIO_PORTA  0
+#define GPIO_PORTB  1
+#define GPIO_PORTC  2
+#define GPIO_PORTD  3
+#define GPIO_PORTE  4
+#define GPIO_PORTF  5
+#define GPIO_PORTG  6
+// Note: PH0 and PH1 intentionally omitted
 
+#define TOTAL_GPIO_PORT                 (GPIO_PORTG + 1)
+#define TOTAL_GPIO_PINS                 (TOTAL_GPIO_PORT*16)
 
-//                              USART2                   USART6                   USART1
-//#define STM32F4_UART_RXD_PINS { PORT_PIN(GPIO_PORTA, 3) } //, PORT_PIN(GPIO_PORTC,  7), PORT_PIN(GPIO_PORTA, 10) }
-//#define STM32F4_UART_TXD_PINS { PORT_PIN(GPIO_PORTA, 2) } //, PORT_PIN(GPIO_PORTC,  6), PORT_PIN(GPIO_PORTA,  9) }
-// RX                                                    PORT_PIN(GPIO_PORTA, 12), PORT_PIN(GPIO_PORTB,  3)
-// TX                                                    PORT_PIN(GPIO_PORTA, 11), PORT_PIN(GPIO_PORTA, 15)
-// RX                                                                              PORT_PIN(GPIO_PORTB,  7)
-// TX                                                                              PORT_PIN(GPIO_PORTA,  6)
-//#define STM32F4_UART_CTS_PINS {(BYTE)GPIO_PIN_NONE } //, 51, 59} // GPIO_PIN_NONE, D3, D11
-//#define STM32F4_UART_RTS_PINS {(BYTE)GPIO_PIN_NONE } //, 52, 60} // GPIO_PIN_NONE, D4, D12
+#define PORT_PIN(port,pin)              (((int)port)*16 + (pin))
+#define _P(port, pin)                   PORT_PIN(GPIO_PORT##port, pin)
+#define _P_NONE_                        GPION_PIN_NONE
 
-// FIXME: Pin Configuration
-#define STM32F4_ADC 3
-#define STM32F4_AD_CHANNELS {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+// Serial
+#define TOTAL_USART_PORT                1
 
-#define STM32F4_PWM_TIMER {4,4,4,4}
-#define STM32F4_PWM_CHNL  {0,1,2,3}
-#define STM32F4_PWM_PINS  {60,61,62,63} // PD12-PD15
+//                                         "COM1"
+//                                         USART3
+#define STM32F7_UART_TXD_PINS           { _P(D, 8) }
+#define STM32F7_UART_RXD_PINS           { _P(D, 9) }
 
-#define STM32F4_SPI_SCLK_PINS {5, 29, 42} // PA5, PB13, PC10
-#define STM32F4_SPI_MISO_PINS {6, 30, 43} // PA6, PB14, PC11
-#define STM32F4_SPI_MOSI_PINS {7, 31, 44} // PA7, PB15, PC12
+// USART1 cannot be used: USART1_RX on PA10 conflicts with USB, PB7 conflicts with LED2
+// USART3 is connected to ST-LINK (PD8, PD9) and available as a virtual COM port
 
-#define STM32F4_I2C_PORT     1
-#define STM32F4_I2C_SCL_PIN  PORT_PIN( GPIO_PORTB, 8 ) // PB8
-#define STM32F4_I2C_SDA_PIN  PORT_PIN( GPIO_PORTB, 9 ) // PB9
-
+// User & Wake-up Button
+#define USER_BUTTON                     _P(C,13)
 
 // User LEDs
-#define LED1                            PORT_PIN(GPIO_PORTB,  0)  // Green
-#define LED2                            PORT_PIN(GPIO_PORTB,  7)  // Blue
-#define LED3                            PORT_PIN(GPIO_PORTB, 14)  // Red
+#define LED1                            _P(B, 0)  // Green
+#define LED2                            _P(B, 7)  // Blue
+#define LED3                            _P(B,14)  // Red
 
 // TinyBooter entry using GPIO
-#define TINYBOOTER_ENTRY_GPIO_PIN       PORT_PIN(GPIO_PORTC, 13)  // 'User' button
+#define TINYBOOTER_ENTRY_GPIO_PIN       USER_BUTTON               // 'User' button
 #define TINYBOOTER_ENTRY_GPIO_STATE     TRUE                      // Active high
 #define TINYBOOTER_ENTRY_GPIO_RESISTOR  RESISTOR_DISABLED         // No internal resistor, there is external pull-down (R58,R59)
 
 //
-// constants
+// Constants
 /////////////////////////////////////////////////////////
 
 #include <processor_selector.h>
